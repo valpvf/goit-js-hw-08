8591ff5cd90cf3072e0b5966d71e5678e65b67a0
Valerii Pozhydaiev <p_vf@i.ua>
2023-03-31 00:23:56 +0300

    task 1&2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+getTimeFromLS('videoplayer-current-time');
+
+player.on('timeupdate', throttle(onSaveLS, 1000));
+
+function onSaveLS(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+function getTimeFromLS(key) {
+  const savedTime = localStorage.getItem(key) || 0;
+  player.setCurrentTime(savedTime);
+}
