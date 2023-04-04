624d8fe0201ee79dd2e85eb9ff547c489068e224
Valerii Pozhydaiev <p_vf@i.ua>
2023-04-05 01:30:57 +0300

    correct mistakes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,36 +2,34 @@ import throttle from 'lodash.throttle';
 
 const refs = {
   formEl: document.querySelector('.feedback-form'),
-  fieldsInputs: {},
-  key: 'feedback-form-state',
 };
+
+let fieldsInputs = {};
+const key = 'feedback-form-state';
 
 refs.formEl.addEventListener('input', throttle(onFormInput, 500));
 refs.formEl.addEventListener('submit', onFormSubmit);
 
 function onStart() {
-  refs.fieldsInputs = JSON.parse(localStorage.getItem(refs.key)) || {};
-  if (refs.fieldsInputs === undefined) {
+  fieldsInputs = JSON.parse(localStorage.getItem(key)) || {};
+  if (!fieldsInputs) {
     return;
   }
-  for (const key in refs.fieldsInputs) {
-    refs.formEl[key].value = refs.fieldsInputs[key];
+  for (const key in fieldsInputs) {
+    refs.formEl[key].value = fieldsInputs[key];
   }
 }
 
 onStart();
 
 function onFormInput(evt) {
-  refs.fieldsInputs[evt.target.name] = evt.target.value;
-  localStorage.setItem(refs.key, JSON.stringify(refs.fieldsInputs));
+  fieldsInputs[evt.target.name] = evt.target.value;
+  localStorage.setItem(key, JSON.stringify(fieldsInputs));
 }
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-  if (evt.target !== evt.currentTarget) {
-    return;
-  }
-  console.log(refs.fieldsInputs);
+  console.log(fieldsInputs);
   evt.currentTarget.reset();
-  localStorage.removeItem(refs.key);
+  localStorage.removeItem(key);
 }
